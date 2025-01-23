b19cbe43598e8cc3c00224bdd1d38704e088f79b
2DAM-adrianrod493 <arodrod0708@g.educaand.es>
2025-01-23 18:20:56 +0100

    hola

---
+++ G4-TaskManager/G4-TaskManager/Views/LoginView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace G4_TaskManager.Views
+{
+    public partial class LoginView : Window
+    {
+        public LoginView()
+        {
+            InitializeComponent();
+        }
+
+        private void IniciarSesion_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+    }
+}
