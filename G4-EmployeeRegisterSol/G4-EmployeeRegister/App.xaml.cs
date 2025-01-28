4c1fcfd0177223fd06ed023225fe5af3638bc0d7
2DAM-adrianrod493 <arodrod0708@g.educaand.es>
2025-01-28 21:05:23 +0100

    todo

---
+++ G4-EmployeeRegisterSol/G4-EmployeeRegister/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace G4_EmployeeRegister
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
