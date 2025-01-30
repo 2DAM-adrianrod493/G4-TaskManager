e2872665e0c130a769e9857639285df22dc3be21
2DAM-adrianrod493 <arodrod0708@g.educaand.es>
2025-01-30 20:31:36 +0100

    todo1

--- G4-EmployeeRegister/G4-EmployeeRegister/ViewModels/AdminViewModel.cs
+++ G4-EmployeeRegister/G4-EmployeeRegister/ViewModels/AdminViewModel.cs
@@ -7,6 +7,8 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
 
 namespace G4_EmployeeRegister.ViewModels
 {
@@ -20,26 +22,27 @@ namespace G4_EmployeeRegister.ViewModels
 
         public ObservableCollection<UsuarioModel> Usuarios { get; set; }
 
-        private string _nombre, _apellido, _rol;
+        #region PROPIEDADES DE USUARIOS
+        private string _nombre, _id, _rol;
         public string Nombre
         {
             get { return _nombre; }
             set
             {
                 _nombre = value;
-                // OnPropertyChanged(nameof(Nombre));
+                OnPropertyChanged(nameof(Nombre));
             }
         }
-        public string Apellido
+        public string Id
         {
             get
             {
-                return _apellido;
+                return _id;
             }
             set
             {
-                _apellido = value;
-                // OnPropertyChanged(nameof(Apellido));
+                _id = value;
+                OnPropertyChanged(nameof(Id));
             }
         }
         public string Rol
@@ -53,6 +56,51 @@ namespace G4_EmployeeRegister.ViewModels
                 _rol = value;
             }
         }
+        #endregion
+
+        #region PÁGINA EDICIÓN
+        private Page _editPage;
+        public Page EditPage
+        {
+            get => _editPage;
+            set
+            {
+                _editPage = value;
+                OnPropertyChanged(nameof(EditPage));
+            }
+        }
+        #endregion
+
+        #region CONTROL VISIBILIDAD
+        private Visibility _editFrameVisibility = Visibility.Hidden;
+        public Visibility EditFrameVisibility
+        {
+            get => _editFrameVisibility;
+            set
+            {
+                _editFrameVisibility = value;
+                OnPropertyChanged(nameof(EditFrameVisibility));
+            }
+        }
+
+        private Visibility _productFrameVisibility = Visibility.Visible;
+        public Visibility ProductFrameVisibility
+        {
+            get => _productFrameVisibility;
+            set
+            {
+                _productFrameVisibility = value;
+                OnPropertyChanged(nameof(ProductFrameVisibility));
+            }
+        }
+        #endregion
+
+        #region COMANDOS
+        public RelayCommand AddUserCommand { get; }
+        public RelayCommand DeleteCommand { get; }
+        public RelayCommand EditCommand { get; }
+        public RelayCommand VolverAtrasCommand { get; }
+        #endregion
 
         public AdminViewModel()
         {
@@ -60,15 +108,90 @@ namespace G4_EmployeeRegister.ViewModels
             _usuariosService = new Services.UsuarioService();
             Usuarios = new ObservableCollection<UsuarioModel>();
 
+            AddUserCommand = new RelayCommand(
+                _ => AddUsuario(),
+                _ => true
+                );
+
+            DeleteCommand = new RelayCommand(
+                paramProducto => DeleteUser(paramProducto),
+                _ => true
+            );
+
+            EditCommand = new RelayCommand(
+                paramProducto => EditProduct(paramProducto),
+                _ => true
+            );
+
+            VolverAtrasCommand = new RelayCommand(
+                _ => VolverAtras(),
+                _ => EditPage != null
+            );
+
+            LoadData();
+
             LoadUsers();
 
         }
 
+        #region MÉTODOS
+        private void EditUser(object objprodd)
+        {
 
+            UsuarioModel usuario = (UsuarioModel)objprodd;
+
+            UsuarioViewModel framePadre = this;
+
+            EditPage = new G4_EmployeeRegister.Views.EditUsuarioView(producto, framePadre, _usuarioServices);
+
+            EditFrameVisibility = Visibility.Visible;
+            ProductFrameVisibility = Visibility.Hidden;
+
+        }
+
+        private void DeleteUser(object usuario)
+        {
+            UsuarioModel usuarioEliminar = (UsuarioModel)usuario;
+
+            var confirmacion = MessageBox.Show("¿Desea eliminar el producto?",
+              "Alerta", MessageBoxButton.OKCancel);
+            if (confirmacion == MessageBoxResult.OK)
+            {
+                _usuarioServices.RemoveProduct(usuarioEliminar);
+            }
+        }
+
+        private void AddUser()
+        {
+            int id = Usuarios.Count + 1;
+            UsuarioModel producto = new UsuarioModel(Id, Nombre, Rol);
+            Usuarios.Add(producto);
+        }
+
+        public void VolverAtras()
+        {
+            EditPage = null;
+            EditFrameVisibility = Visibility.Hidden;
+            ProductFrameVisibility = Visibility.Visible;
+        }
+
+        private void LoadData()
+        {
+            Usuarios = _usuarioServices.GetAllUsuarios();
+        }
+        #endregion
 
         private void LoadUsers()
         {
             Usuarios = new UsuarioService().GetAllUsuarios();
         }
+
+        #region EVENTOS DE NOTIFICACIÓN
+        protected void OnPropertyChanged(string propName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+        #endregion
+
     }
 }
